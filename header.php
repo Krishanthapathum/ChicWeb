889e756ee59c0d501a1426aa90648080d2b7cb4f
Krishanthapathum <87825292+Krishanthapathum@users.noreply.github.com>
2023-12-09 03:13:40 +0530

    Develop category

--- header.php
+++ header.php
@@ -1,11 +1,15 @@
 <div class="container">
-	<div class="header">
-    	<div class="logo">
-        <a href="index.php"><img src="images/logo.jpg" alt="Chic Clothing logo" /></a>
+    <div class="header">
+        <div class="logo">
+            <a href="index.php"><img src="images/logo.jpg" alt="Chic Clothing logo" /></a>
         </div><!--logo ends-->
-		<div class="navigation">
-		<p>Links go here
-        <a href="admin.php">Admin</a>
-        </p>
-      </div><!--navigation ends-->
-	</div><!-- Header ends here-->
+        <div class="navigation">
+
+        <p> <?php
+           include("categoryList.php");
+                ?>
+
+                <a href="admin.php">Admin</a>
+            </p>
+        </div><!--navigation ends-->
+    </div><!-- Header ends here-->
