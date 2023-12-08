1f6b737bf2b677be49b5af46aac1dc49e04ed0eb
Krishanthapathum <87825292+Krishanthapathum@users.noreply.github.com>
2023-12-09 03:41:13 +0530

    modify

--- index.php
+++ index.php
@@ -1,31 +1,43 @@
 <?php
 //This file is the base for all pages in the site. When creating a new page, we just open this one, then save a copy as the new page.
- include("dbconnect.php");
+include("dbconnect.php");
 ?>
 <html>
 
 <head>
-  <title>Welcome to Chic Clothing</title>
+    <title>Welcome to Chic Clothing</title>
 
-  <link href="styles.css" rel="stylesheet" type="text/css" />
+    <link href="styles.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
+    <div class="container">
+        <?php
+        include("header.php");
+        //check to see if user is visiting a page other than the home page
+        if (isset($_GET['page'])) {
+        ?><div class="banner"><img src="images/banner.jpg" alt="chic clothes" /></div>
+        <?php
+        }
+        ?>
+        <div class="maincontent">
+            <!-- main content goes here-->
+            <?php
+            if (!isset($_GET['page'])) {
+                include("home.php");
+            } else {
+                $page = $_GET['page'];
+                include("$page.php");
+            }
+            ?>
+        </div>
 
-  <?php
-  include("header.php");
-  ?>
+        <?php
+        include("seccontent.php");
+        ?>
 
-  <div class="maincontent">
-    <!-- main content goes here-->
-    <p>Lorem ipsum goes here</p>
-  </div>
-
-  <?php
-  include("seccontent.php");
-  ?>
-
-  <div class="footer"></div>
-  </div><!-- Container ends here-->
+        <div class="footer"></div>
+    </div><!-- Container ends here-->
 </body>
+
 </html>
